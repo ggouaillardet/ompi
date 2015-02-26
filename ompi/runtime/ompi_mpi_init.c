c0f3d1ea4a5a9b3a07ccdbef64de0b5f44f8c87a
Gilles Gouaillardet <gilles.gouaillardet@iferc.org>
2015-02-26 17:31:17 +0900

    fix f08 bindings
    only define the unique fortran symbol depending on
     - CAPS
     - PLAIN
     - SINGLE_UNDERSCORE
     - DOUBLE_UNDERSCORE
    and bind the f08 symbol to the uniquely defined C symbol

--- ompi/runtime/ompi_mpi_init.c
+++ ompi/runtime/ompi_mpi_init.c
@@ -17,7 +17,7 @@
  * Copyright (c) 2011      Sandia National Laboratories. All rights reserved.
  * Copyright (c) 2012-2013 Inria.  All rights reserved.
  * Copyright (c) 2014      Intel, Inc. All rights reserved.
- * Copyright (c) 2014      Research Organization for Information Science
+ * Copyright (c) 2014-2015 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  *
  * $COPYRIGHT$
@@ -256,11 +256,22 @@ MPI_Fint *MPI_F_STATUSES_IGNORE = NULL;
    ompi/include/mpif-common.h.
  */
 
+#if OMPI_BUILD_FORTRAN_BINDINGS
+#  if OMPI_FORTRAN_CAPS
 #define INST(type, upper_case, lower_case, single_u, double_u)   \
-type lower_case; \
-type upper_case; \
-type single_u;  \
+type lower_case
+#  elif OMPI_FORTRAN_PLAIN
+#define INST(type, upper_case, lower_case, single_u, double_u)   \
+type upper_case
+#  elif OMPI_FORTRAN_SINGLE_UNDERSCORE
+#define INST(type, upper_case, lower_case, single_u, double_u)   \
+type single_u
+#  elif OMPI_FORTRAN_DOUBLE_UNDERSCORE
+#define INST(type, upper_case, lower_case, single_u, double_u)   \
 type double_u
+#  else
+#    error Unrecognized Fortran name mangling scheme
+#  endif
 
 INST(int, MPI_FORTRAN_BOTTOM, mpi_fortran_bottom,
      mpi_fortran_bottom_, mpi_fortran_bottom__);
@@ -280,6 +291,7 @@ INST(int *, MPI_FORTRAN_STATUS_IGNORE, mpi_fortran_status_ignore,
      mpi_fortran_status_ignore_, mpi_fortran_status_ignore__);
 INST(int *, MPI_FORTRAN_STATUSES_IGNORE, mpi_fortran_statuses_ignore,
       mpi_fortran_statuses_ignore_, mpi_fortran_statuses_ignore__);
+#endif  /* OMPI_BUILD_FORTRAN_BINDINGS */
 
 /*
  * Hash tables for MPI_Type_create_f90* functions
